b5e2ad9842eb2a063431e40eee47e5c74744ff1d
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-07-29 00:18:17 +0500

    Creating a React Component

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+// PascalCasing
+function Message() {
+  // JSX: JavaScript XML
+  const name = "Maaz Ur Rahman";
+  if (name) {
+    return <h1>Hello {name}</h1>;
+  }
+  return <h1>Hello World</h1>;
+}
+
+export default Message;
