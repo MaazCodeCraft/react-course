adcacd2d0bd97db3d230937340eb910ca449ff58
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-08-07 06:16:17 +0500

    Exercise-3 Updating State

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 
 function App() {
-  const [pizza, setPizza] = React.useState({
-    name: "Spicy Pepperoni",
-    topping: ["Mushroom"],
+  const [cart, setCart] = React.useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({
-      ...pizza,
-      topping: [...pizza.topping, "Cheese"],
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
     });
   };
 
