1674fe8dd987296035b88b84ec94b5a099fb7c3e
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-08-09 05:54:14 +0500

    Updating Arrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 function App() {
-  const [bugs, setBugs] = React.useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
-  ]);
+  const [tags, setTags] = React.useState(["happy", "cheerful"]);
 
   const handleClick = () => {
-    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    // Add
+    setTags([...tags, "exciting"]);
+
+    // remove
+    setTags(tags.filter((tag) => tag !== "happy"));
+
+    // Update
+    setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
   };
 
   return <></>;
