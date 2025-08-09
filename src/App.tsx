82e9f24e7fd9aa07abd37eee53cfe93d9f22eaba
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-08-09 05:50:37 +0500

    Updating Array of Objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import Message from "./Message";
+import React from "react";
 
 function App() {
-  return (
-    <>
-      <Message />
-    </>
-  );
+  const [bugs, setBugs] = React.useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
+
+  const handleClick = () => {
+    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+  };
+
+  return <></>;
 }
 
 export default App;
