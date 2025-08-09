944c2bc01885142114997415610b1a95780124a5
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-08-09 06:09:39 +0500

    Updating Nested Objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
-import { produce } from "immer";
 import React from "react";
 
 function App() {
-  const [bugs, setBugs] = React.useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
-  ]);
+  const [customer, setCustomer] = React.useState({
+    name: "Jhon",
+    address: {
+      city: "Tokyo",
+      zipCode: 9822,
+    },
+  });
 
   const handleClick = () => {
-    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
-    setBugs(
-      produce((draft) => {
-        const bug = draft.find((bug) => bug.id === 1);
-        if (bug) {
-          bug.fixed = true;
-        }
-      })
-    );
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipCode: 1234 },
+    });
   };
 
   return (
     <div>
-      {bugs.map((bug) => (
-        <p key={bug.id}>
-          {bug.title} {bug.fixed ? "Fixed" : "New"}
-        </p>
-      ))}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
