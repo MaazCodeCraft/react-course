e1ec4abca3f692a341576137eee22c52deaf1c9c
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-08-12 05:56:39 +0500

    Building a Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <>
-      <ExpandableText>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione
-        suscipit deserunt facilis, tenetur expedita, dolores omnis nemo ex,
-        eveniet minima qui aliquam quam alias voluptate earum totam cumque rem?
-        Voluptates aperiam debitis unde accusamus adipisci recusandae laborum
-        enim laudantium corporis eum tenetur est ipsa natus, facilis facere
-        illum tempore esse dicta. Excepturi obcaecati quos non illo temporibus
-        possimus expedita, voluptatum amet quod impedit numquam consequatur
-        voluptatibus libero placeat officiis dolor vero illum labore assumenda
-        cupiditate laudantium rem nostrum voluptas. Nulla deleniti dolorum natus
-        provident, unde pariatur ipsa voluptatem quos. Distinctio optio ratione
-        amet numquam delectus maiores temporibus deleniti reprehenderit porro.
-      </ExpandableText>
-      <ExpandableText>Hello World</ExpandableText>
+      <Form />
     </>
   );
 }
