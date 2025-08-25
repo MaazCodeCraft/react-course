dacee73e3e79c3eaecfbb116f98bda31d5217ab7
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-08-25 12:10:52 +0500

    Adding an Expense and Save data in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,33 @@
-import { useState } from "react";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
+import { useEffect, useState } from "react";
+import ExpenseList, {
+  type Expense,
+} from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import catergories from "./expense-tracker/categories";
 
 function App() {
   const [selectCategory, setSelectCategory] = useState("");
-  const [expenses, setExpenses] = useState([
-    {
-      id: 1,
-      description: "aaa",
-      amount: 10,
-      category: "Utilities",
-    },
-    {
-      id: 2,
-      description: "bbb",
-      amount: 10,
-      category: "Utilities",
-    },
-    {
-      id: 3,
-      description: "ccc",
-      amount: 10,
-      category: "Utilities",
-    },
-    {
-      id: 4,
-      description: "ddd",
-      amount: 10,
-      category: "Utilities",
-    },
-  ]);
+  const [expenses, setExpenses] = useState<Expense[]>(() => {
+    const saved = localStorage.getItem("expenses");
+    return saved ? JSON.parse(saved) : [];
+  });
 
   const visibleExpenses = selectCategory
     ? expenses.filter((e) => e.category === selectCategory)
     : expenses;
 
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
+
   return (
     <>
       <div className="mb-5">
-        <ExpenseForm />
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
       </div>
       <div className="mb-3">
         <ExpenseFilter
